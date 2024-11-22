758078d139945cc5a855a483876748ca7a13f936
SrinathR8 <srinath.r8@gmail.com>
2024-11-22 16:25:59 +0530

    Update interactive-v3.py

--- interactive-v3.py
+++ interactive-v3.py
@@ -78,7 +78,7 @@ if st.session_state.task == "Screening chatbot":
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if st.session_state.mydf.any():
+    if st.session_state.mydf is not None:
         user_prompt = st.chat_input("Enquire about the candidates")  
 
     
